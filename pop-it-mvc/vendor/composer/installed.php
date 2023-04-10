cc5c4993437fc01c474ad5d7228ea764f54d2d39
Дмитрий Никитин <gr404_nidal@lab-407-8.tomtit-tomsk.ru>
2023-04-10 11:30:22 +0700

    устоновлен компосер

---
+++ pop-it-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'franky5326/pncc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a38a81493bd511db1e354377c7a167f722b857d4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'franky5326/pncc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a38a81493bd511db1e354377c7a167f722b857d4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
